ade5f166bc184fd3474384508d125770e132f6db
GeraldAkenji <akenjigerald@gmail.com>
2025-01-25 12:13:51 -0700

    three private subnets across different availability zones

--- main.tf
+++ main.tf
@@ -33,3 +33,30 @@ resource "aws_subnet" "public_subnet_3" {
     Name = "Public Subnet 3"
   }
 }
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_cidrs[0]
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "Private Subnet 1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_cidrs[1]
+  availability_zone = "us-west-2b"
+  tags = {
+    Name = "Private Subnet 2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_cidrs[2]
+  availability_zone = "us-west-2c"
+  tags = {
+    Name = "Private Subnet 3"
+  }
+}
